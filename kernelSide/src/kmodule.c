187e1e9aca5126709ed35728e5924eb0c7d04439
danandla <d.a.n.202021@gmail.com>
2022-12-16 18:00:20 +0300

    for danya never asked

--- kernelSide/src/kmodule.c
+++ kernelSide/src/kmodule.c
@@ -8,6 +8,9 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/string.h>
+#include <linux/path.h>
+#include <linux/namei.h>
+#include <linux/dcache.h>
 
 
 MODULE_LICENSE("Dual BSD/GPL");
@@ -16,22 +19,88 @@ MODULE_VERSION("1.0");
 #define WBUFFER_SIZE 2000
 #define RBUFFER_SIZE 2000
 
-static char data_wbuffer[WBUFFER_SIZE];
-static char data_rbuffer[RBUFFER_SIZE];
 int writelen;
+struct dentry* direction_entry;
 
 static ssize_t _proc_read(struct file *f, char __user *buffer, size_t len, loff_t *offset){
-    loff_t msg_len = 0;
+    char data_rbuffer[RBUFFER_SIZE] = {0};
+    char isOk;
+    isOk = 0;
 
-    msg_len += sprintf(data_rbuffer + msg_len, "hello from kernel\n");
+    loff_t msg_len = 0;
+    if (*offset > 0) return 0;
+
+    if(direction_entry != NULL){
+        if(direction_entry->d_inode != NULL){
+            /*
+            msg_len += sprintf(data_rbuffer + msg_len, "----DENTRY INFO----\n");
+            msg_len += sprintf(data_rbuffer + msg_len, "d_iname: %s\n", direction_entry->d_iname);
+            msg_len += sprintf(data_rbuffer + msg_len, "inode number: %lu\n", direction_entry->d_inode->i_ino);
+            msg_len += sprintf(data_rbuffer + msg_len, "inode file size: %lld\n", direction_entry->d_inode->i_size);
+            msg_len += sprintf(data_rbuffer + msg_len, "dflags: %lu\n", direction_entry->d_flags);
+            msg_len += sprintf(data_rbuffer + msg_len, "d_time: %lu\n", direction_entry->d_time);
+            msg_len += sprintf(data_rbuffer + msg_len, "superblock size: %lu\n", direction_entry->d_sb->s_blocksize);
+            msg_len += sprintf(data_rbuffer + msg_len, "-------------\n"); 
+            */
+            /*
+            struct user_dentry{
+                unsigned char * d_iname;
+            };
+            struct user_dentry u_dentry;
+            u_dentry = (struct user_dentry){
+                .d_iname = direction_entry->d_iname
+            };
+            memcpy(data_rbuffer, &u_dentry, sizeof(u_dentry));
+            msg_len += sizeof(u_dentry);
+            */
+            struct user_dentry{
+                //unsigned char d_iname[DNAME_INLINE_LEN]
+                unsigned int d_flags;   
+            };
+            struct user_dentry u_dentry;
+            u_dentry = (struct user_dentry){
+                .d_flags = direction_entry->d_flags
+            //    .d_iname = direction_entry->d_iname
+            };
+            isOk = 1;
+            memcpy(data_rbuffer, &isOk, sizeof(char));
+            msg_len += sizeof(char);
+            memcpy(data_rbuffer + msg_len, &u_dentry, sizeof(struct user_dentry));
+            msg_len += sizeof(u_dentry);
+            pr_info("dentry wrote.");
+        } else{
+            pr_info("inode wasn't written.");
+        }
+    } else{
+        msg_len = 0;
+        msg_len += sprintf(data_rbuffer + msg_len, "can't acces structure\n");
+    }
     return simple_read_from_buffer(buffer, msg_len, offset, data_rbuffer, RBUFFER_SIZE);
 }
 
 static ssize_t _proc_write(struct file *f, const char __user *buffer, size_t len, loff_t *offset){
     ssize_t ret;
+    struct path p;
+    int err;
 
+    char data_wbuffer[WBUFFER_SIZE] = {0};
+    //memset(data_wbuffer, 0, sizeof(WBUFFER_SIZE));
     ret = simple_write_to_buffer(data_wbuffer, WBUFFER_SIZE, offset, buffer, len);
-    if (ret > 0) writelen = max_t(int, writelen, *offset);
+    if (ret > 0){
+        // writelen = max_t(int, writelen, *offset);
+        writelen = *offset;
+        printk(KERN_INFO " writeLen: %d\n", writelen); 
+
+        // err = kern_path("/home/adi/ak", LOOKUP_FOLLOW, &p);
+        err = kern_path(data_wbuffer, LOOKUP_FOLLOW, &p);
+        if(err != 0) {
+            printk(KERN_ALERT "invalid path: %s, err: %d\n", data_wbuffer, err); 
+            direction_entry = NULL;
+            return err;
+        }
+        direction_entry = p.dentry;
+        printk(KERN_INFO "valid path: %s\n", data_wbuffer); 
+    }
 
     return ret;    
 }
@@ -46,6 +115,7 @@ static void create_entry(void){
 }
 
 static int __init sreader_init(void){
+    writelen = 0;
     printk(KERN_INFO "sreader: module loading\n");
     create_entry();
     return 0;
--- userSide/src/umodule.c
+++ userSide/src/umodule.c
@@ -1,20 +1,92 @@
+#include <linux/fs.h>
 #include <string.h>
-#include <stdio.h>
+#include <stdio.h> 
+#include <stdlib.h>
 
 #define OUTPUT_BUFFER_SIZE 2000
+#define INPUT_BUFFER_SIZE 2000
 #define KMODULE "/proc/sreader"
 
-int main() {
 
+struct user_dentry{
+   unsigned int  d_flags;
+};
+
+void print_dentry(struct user_dentry* direction_entry){
+    printf("----DENTRY INFO----\n");
+        //printf("d_iname: %s\n", direction_entry->d_iname);
+        printf("dflags: %lu\n", direction_entry->d_flags);
+        //printf("d_time: %ul\n", direction_entry->d_time);
+        //printf("superblock: %ul\n", direction_entry->d_sb->s_root->d_iname);
+    printf("-------------\n"); 
+}
+
+
+int main(int args, char **argv) {
     char output[OUTPUT_BUFFER_SIZE];
+    char input[INPUT_BUFFER_SIZE];
+
+    if (args != 2) {
+        printf("Wrong number of arguments: %d. Requiered: 1\n", args);
+        return 1;
+    }
+
+
+    sprintf(input, "%s", argv[1]);
+
+    const char * end_marker = "-------------\n";
+
+    FILE *w_file;
+    w_file = fopen(KMODULE, "w");
+    if(w_file == NULL){
+        printf("can't open file\n");
+        return 1;
+    }
+    fprintf(w_file, "%s", input);
+
 
     FILE *r_file;
-
     r_file = fopen(KMODULE, "r");
+    if(r_file == NULL){
+        printf("can't open file\n");
+        return 1;
+    }
+    char isOk;
+    if (fread(&isOk, sizeof(char), 1, r_file) == 0){
+        printf("can't read structure from file\n");
+    } else{
+        printf("file found!\n");
+        struct user_dentry den;
+        unsigned int str;
+        if (fread(&den, sizeof(struct user_dentry), 1, r_file) == 0){
+            printf("can't read structure from file\n");
+        }
+        else{
+            //printf("d_iname: %u\n", str);
+            print_dentry(&den); 
+        }
+        fclose(r_file);
+    }
+
+
+    //struct inode* ind = (struct inode*) malloc(640);
+
+    //r_file = fopen(KMODULE, "r");
+    /*
+    if (fread(&den, sizeof(struct user_dentry), 1, r_file) == 0){
+        printf("can't read structure from file\n");
+    }
+    else{
     while (fgets(output, 256, r_file) != NULL) {
         printf("%s", output);
+        if(strcmp(output, end_marker) == 0) break;
     }
-    fclose(r_file);
-
+    */
+    /*
+    fgets(output, 256, r_file);
+    memcpy(ind, output, sizeof(struct user_dentry));
+    print_dentry(ind); 
+    */
+    //fgets(output, 256, r_file);
     return 0;
 }
